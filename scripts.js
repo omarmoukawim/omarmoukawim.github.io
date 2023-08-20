3e18281da15618e4eba7c4a590b6a76796d5c65f
Omar Moukawim <109683799+omarmoukawim@users.noreply.github.com>
2023-08-20 20:58:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,26 +3,31 @@ const projectList = document.getElementById('project-list');
 
 const githubUsername = 'omarmoukawim';
 
-// Fetch GitHub repository data
-fetch(`https://api.github.com/users/${githubUsername}/repos`)
+// Fetch and display GitHub projects
+fetch('https://api.github.com/users/${githubUsername}/repos?sort=created&per_page=6')
     .then(response => response.json())
     .then(data => {
-        const projects = data.filter(repo => !repo.fork);
-        projectList.innerHTML = projects.map(project => `
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">${project.name}</h5>
-                        <p class="card-text">${project.description || 'No description available'}</p>
-                        <a href="${project.html_url}" target="_blank" class="btn btn-primary">View on GitHub</a>
-                    </div>
-                </div>
-            </div>
-        `).join('');
+        const projectList = document.getElementById('project-list');
+
+        data.forEach(repo => {
+            // Fetch README content
+            fetch(`https://api.github.com/repos/${githubUsername}/${repo.name}/readme`)
+                .then(response => response.json())
+                .then(readmeData => {
+                    const card = document.createElement('div');
+                    card.classList.add('col-md-4', 'project-card');
+                    card.innerHTML = `
+                        <h5>${repo.name}</h5>
+                        <p>${readmeData.content ? atob(readmeData.content) : 'No description available.'}</p>
+                        <a href="${repo.html_url}" target="_blank" class="btn btn-dark">View Repo</a>
+                    `;
+                    projectList.appendChild(card);
+                })
+                .catch(error => console.error('Error fetching README:', error));
+        });
     })
-    .catch(error => {
-        console.error('Error fetching GitHub data:', error);
-    });
+    .catch(error => console.error('Error fetching projects:', error));
+
 
 // Smooth scrolling behavior for navigation links
 document.querySelectorAll('nav a').forEach(anchor => {
