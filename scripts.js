0cebf2ec85f44d190b46d6e766e8156f0db2c719
Omar Moukawim <109683799+omarmoukawim@users.noreply.github.com>
2023-08-21 15:27:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -48,3 +48,46 @@ document.querySelectorAll('a.nav-link, a.timeline-link').forEach(anchor => {
     });
 });
 
+
+const workExperiences = [
+    {
+        title: "Master Thesis | Cybersecurity for Automotive",
+        company: "Leonardo",
+        location: "L'Aquila, Italy",
+        date: "July 2023 - Present",
+        description: "My work revolves around ensuring the Cybersecurity of vehicles, encompassing the protection...",
+    },
+    {
+        title: "Intern | Core System Application Integration",
+        company: "Volvo Cars Corporation",
+        location: "Göteborg, Sweden",
+        date: "January 2023 - May 2023",
+        description: "I worked with further development of test activities to enable test automation...",
+    },
+    {
+        title: "ICT Internship",
+        company: "Vantea Spa",
+        location: "Rome, Italy",
+        date: "April 2021 - July 2021",
+        description: "Main goals:\n- Project and realization of specific Software solutions with languages of scripting...",
+    },
+    // Add more work experiences here
+];
+
+const timeline = document.querySelector(".timeline");
+
+workExperiences.forEach((experience) => {
+  const timelineItem = document.createElement("div");
+  timelineItem.classList.add("timeline-item");
+
+  timelineItem.innerHTML = `
+    <div class="timeline-item-content">
+        <h4>${experience.title}</h4>
+        <p class="timeline-item-subtitle">${experience.company} | ${experience.location}</p>
+        <p class="timeline-item-date">${experience.date}</p>
+        <p>${experience.description}</p>
+    </div>
+  `;
+
+  timeline.appendChild(timelineItem);
+});
