0df390892016c2b7734306bb3543fa73dc4dbea9
Omar Moukawim <109683799+omarmoukawim@users.noreply.github.com>
2023-08-21 13:50:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Fetch project data from your GitHub repository
-  fetch("https://api.github.com/users/omarmoukawim/repos")
-    .then((response) => response.json())
-    .then((data) => {
-      const projectList = document.getElementById("project-list");
+    // Fetch project data from your GitHub repository
+    fetch("https://api.github.com/users/omarmoukawim/repos")
+        .then((response) => response.json())
+        .then((data) => {
+            const projectList = document.getElementById("project-list");
 
-      data.forEach((repo) => {
-        const projectCard = document.createElement("div");
-        projectCard.classList.add("col-md-4", "project-card");
+            data.forEach((repo) => {
+                const projectCard = document.createElement("div");
+                projectCard.classList.add("col-md-4", "project-card");
 
-        projectCard.innerHTML = `
-          <h3>${repo.name}</h3>
-          <p>${repo.description || "No description available."}</p>
-          <a href="${repo.html_url}" target="_blank">View on GitHub</a>
-        `;
+                projectCard.innerHTML = `
+                    <h3>${repo.name}</h3>
+                    <p>${repo.description || "No description available."}</p>
+                    <a href="${repo.html_url}" target="_blank">View on GitHub</a>
+                `;
 
-        projectList.appendChild(projectCard);
-      });
-    })
-    .catch((error) => console.error("Error fetching project data:", error));
+                projectList.appendChild(projectCard);
+            });
+        })
+        .catch((error) => console.error("Error fetching project data:", error));
 });
 
 // Smooth scrolling for navigation links
